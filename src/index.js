759786d9a83203168e0f7fb2729e010834222f5e
dariok87 <dkemenovic@gmail.com>
2022-02-14 21:48:25 +0100

    Reference function in JSX

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  const buttonText = 'Click me'
-
-  return <div>
-    <label className="label">
-      Enter name:
-    </label>
-    <input />
-    <button style={{backgroundColor: 'blue'}}>{buttonText}</button>
-  </div>
+function getButtontext() {
+  return 'Click on me';
 }
+const App = () => {
+  return (
+    <div>
+      <label className="label">
+        Enter name:
+      </label>
+      <input />
+      <button style={{backgroundColor: 'blue'}}>{getButtontext()}</button>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <App />, 
